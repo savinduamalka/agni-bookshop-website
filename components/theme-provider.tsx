a8b74a9e5f7c844e0639de7d328e8e49012ae945
Savindu Amalka <savinduamalka@gmail.com>
2025-12-05 16:23:19 +0530

    Update ThemeProvider to use cookie storage for compatibility in restricted contexts

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,21 @@
-'use client'
+'use client';
 
-import * as React from 'react'
+import * as React from 'react';
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
-} from 'next-themes'
+} from 'next-themes';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      storage="cookie"
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
